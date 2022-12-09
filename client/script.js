64abde3407d2093a168c08a35791e56a261e870f
mdiaby1 <64927434+mdiaby1@users.noreply.github.com>
2022-12-09 04:03:13 -0500

    Update script.js

--- client/script.js
+++ client/script.js
@@ -1,3 +1,5 @@
 // Make the HTTP request to the API
+
 fetch('https://data.princegeorgescountymd.gov/resource/7k64-tdwr.json')
-.then(response => response.json)
+  .then((response) => response.json())
+  .then((data) => console.log(data));
