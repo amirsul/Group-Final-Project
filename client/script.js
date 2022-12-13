dde4d86efe5184697e9b72e74a5d620257c4045b
Amir Sultan <amir.sul98@gmail.com>
2022-12-13 00:02:51 -0500

    undefined

--- client/script.js
+++ client/script.js
@@ -10,6 +10,13 @@ fetch('https://data.princegeorgescountymd.gov/resource/7k64-tdwr.json')
   .then((response) => response.json())
   .then((data) => console.log(data));
 
+/* eslint-disable max-len */
+
+/*
+  Hook this script to index.html
+  by adding `<script src="script.js">` just before your closing `</body>` tag
+*/
+
 /*
   ## Utility Functions
     Under this comment place any utility functions you need - like an inclusive random number selector
@@ -34,6 +41,20 @@ function injectHTML(list) {
     el.innerText = item.name;
     listEl.appendChild(el);
   });
+  /*
+      ## JS and HTML Injection
+        There are a bunch of methods to inject text or HTML into a document using JS
+        Mainly, they're considered "unsafe" because they can spoof a page pretty easily
+        But they're useful for starting to understand how websites work
+        the usual ones are element.innerText and element.innerHTML
+        Here's an article on the differences if you want to know more:
+        https://developer.mozilla.org/en-US/docs/Web/API/Node/textContent#differences_from_innertext
+
+      ## What to do in this function
+        - Accept a list of restaurant objects
+        - using a .forEach method, inject a list element into your index.html for every element in the list
+        - Display the name of that restaurant and what category of food it is
+    */
 }
 
 function processRestaurants(list) {
@@ -45,23 +66,23 @@ function processRestaurants(list) {
   });
   return newArray;
   /*
-          ## Process Data Separately From Injecting It
-            This function should accept your 1,000 records
-            then select 15 random records
-            and return an object containing only the restaurant's name, category, and geocoded location
-            So we can inject them using the HTML injection function
+        ## Process Data Separately From Injecting It
+          This function should accept your 1,000 records
+          then select 15 random records
+          and return an object containing only the restaurant's name, category, and geocoded location
+          So we can inject them using the HTML injection function
 
-            You can find the column names by carefully looking at your single returned record
-            https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
+          You can find the column names by carefully looking at your single returned record
+          https://data.princegeorgescountymd.gov/Health/Food-Inspection/umjn-t2iz
 
-          ## What to do in this function:
+        ## What to do in this function:
 
-          - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
-          - using a .map function on that range,
-          - Make a list of 15 random restaurants from your list of 100 from your data request
-          - Return only their name, category, and location
-          - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
-        */
+        - Create an array of 15 empty elements (there are a lot of fun ways to do this, and also very basic ways)
+        - using a .map function on that range,
+        - Make a list of 15 random restaurants from your list of 100 from your data request
+        - Return only their name, category, and location
+        - Return the new list of 15 restaurants so we can work on it separately in the HTML injector
+      */
 }
 
 function filterList(list, filterInputValue) {
@@ -70,7 +91,7 @@ function filterList(list, filterInputValue) {
     const lowerCaseName = item.name.toLowerCase();
     const lowerCaseQuery = filterInputValue.toLowerCase();
     return lowerCaseName.includes(lowerCaseQuery);
-  });
+  })
 }
 
 function initMap() {
@@ -92,7 +113,7 @@ function markerPlace(array, map) {
   });
 
   array.forEach((item, index) => {
-    const {coordinates} = item.geocoded_column_1;
+    const {coordinates}= item.geocoded_column_1;
     L.marker([coordinates[1], coordinates[0]]).addTo(map);
     if (index === 0) {
       map.setView([coordinates[1], coordinates[0]], 10);
@@ -102,12 +123,12 @@ function markerPlace(array, map) {
 
 async function mainEvent() {
   /*
-          ## Main Event
-            Separating your main programming from your side functions will help you organize your thoughts
-            When you're not working in a heavily-commented "learning" file, this also is more legible
-            If you separate your work, when one piece is complete, you can save it and trust it
-        */
-  const pageMap = initMap();
+        ## Main Event
+          Separating your main programming from your side functions will help you organize your thoughts
+          When you're not working in a heavily-commented "learning" file, this also is more legible
+          If you separate your work, when one piece is complete, you can save it and trust it
+      */
+  const pageMap= initMap();
   // the async keyword means we can make API requests
   const form = document.querySelector('.main_form'); // get your main form so you can do JS with it
   const submit = document.querySelector('#get-resto'); // get a reference to your submit button
@@ -115,30 +136,29 @@ async function mainEvent() {
   submit.style.display = 'none'; // let your submit button disappear
 
   /*
-          Let's get some data from the API - it will take a second or two to load
-          This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
-          It's at about line 27 - go have a look and see what we're retrieving and sending back.
-         */
-  const results = await fetch('/api/foodServicePG');
+        Let's get some data from the API - it will take a second or two to load
+        This next line goes to the request for 'GET' in the file at /server/routes/foodServiceRoutes.js
+        It's at about line 27 - go have a look and see what we're retrieving and sending back.
+       */
+  const results = await fetch('https://data.princegeorgescountymd.gov/resource/7k64-tdwr.json');
   const arrayFromJson = await results.json(); // here is where we get the data from our request as JSON
-
   /*
-          Below this comment, we log out a table of all the results using "dot notation"
-          An alternate notation would be "bracket notation" - arrayFromJson["data"]
-          Dot notation is preferred in JS unless you have a good reason to use brackets
-          The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
-        */
-  console.table(arrayFromJson.data);
+        Below this comment, we log out a table of all the results using "dot notation"
+        An alternate notation would be "bracket notation" - arrayFromJson["data"]
+        Dot notation is preferred in JS unless you have a good reason to use brackets
+        The 'data' key, which we set at line 38 in foodServiceRoutes.js, contains all 1,000 records we need
+      */
+  console.table(arrayFromJson);
 
   // in your browser console, try expanding this object to see what fields are available to work with
   // for example: arrayFromJson.data[0].name, etc
-  console.log(arrayFromJson.data[0]);
+  console.log(arrayFromJson[0]);
 
   // this is called "string interpolation" and is how we build large text blocks with variables
-  console.log(`${arrayFromJson.data[0].name} ${arrayFromJson.data[0].category}`);
+  console.log(`${arrayFromJson[0].name} ${arrayFromJson[0].category}`);
 
   // This IF statement ensures we can't do anything if we don't have information yet
-  if (arrayFromJson.data?.length > 0) { // the question mark in this means "if this is set at all"
+  if (arrayFromJson?.length > 0) { // the question mark in this means "if this is set at all"
     submit.style.display = 'block'; // let's turn the submit button back on by setting it to display as a block when we have data available
 
     // hide load button
@@ -161,7 +181,7 @@ async function mainEvent() {
       submitEvent.preventDefault();
 
       // This constant will have the value of your 15-restaurant collection when it processes
-      currentList = processRestaurants(arrayFromJson.data);
+      currentList = processRestaurants(arrayFromJson);
       console.log(currentList);
       // And this function call will perform the "side effect" of injecting the HTML list for you
       injectHTML(currentList);
@@ -174,8 +194,8 @@ async function mainEvent() {
 }
 
 /*
-        This last line actually runs first!
-        It's calling the 'mainEvent' function at line 57
-        It runs first because the listener is set to when your HTML content has loaded
-      */
+      This last line actually runs first!
+      It's calling the 'mainEvent' function at line 57
+      It runs first because the listener is set to when your HTML content has loaded
+    */
 document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
